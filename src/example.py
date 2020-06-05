8270db91d2715b11ece511e5ad38c68aa3c0f691
牟元凯 <1341301521@qq.com>
2020-06-05 17:10:40 +0800

    inheritance

--- src/example.py
+++ src/example.py
@@ -1,44 +1,31 @@
+from multimethod import multimethod
 
-from src.multimethod import multimethod
 
 # Different methods are used depending on the type of input parameter
 @multimethod(int, int)
 def foo(a, b):
-    return a+b
+    return a + b
+
 
 @multimethod(float, float)
 def foo(a, b):
-    return a-b
+    return a - b
+
 
 @multimethod(str, str)
 def foo(a, b):
-    return a+b
+    return a + b
+
 
 @multimethod(str)
 @multimethod(str, int)
-def foo(a,b=10):
-    c=str(b)
-    return a+c
+def foo(a, b=10):
+    c = str(b)
+    return a + c
 
 
 @multimethod(float)
 @multimethod(float, int)
-@multimethod(float, int,int)
-def foo(a,b=10,c=10):
-    return a+b+c
-
-
-#  Add support for named argumants `foo(a=1, b=2)`.
-@multimethod()
-def foo(a=1,b=2):
-    return a+b
-
-
-
-
-
-
-
-
-
-
+@multimethod(float, int, int)
+def foo(a, b=10, c=10):
+    return a + b + c
--- src/multimethod.py
+++ src/multimethod.py
@@ -6,39 +6,46 @@ class MultiMethod(object):
     def __init__(self, name):
         self.name = name
         self.typemap = {}
-    #Make the object itself callable
-    def __call__(self, *args):
-        types = tuple((arg.__class__) for arg in args) # a generator expression!
-        function = self.typemap.get(types)
-        if function is None:
-            raise TypeError("no match")
-        return function(*args)
-    # Input values are supported as key-value structures
-    def __call__(self,**kwargs):
-        types = tuple((kwargs[key].__class__) for key in kwargs)  # a generator expression!
-        function = self.typemap.get(types)
-        if function is None:
-            raise TypeError("no match")
-        return function(**kwargs)
 
-    #Add a new function to the type diagram
+    # Make the object itself callable
+    def __call__(self, *args, **kwargs):
+        function = self.check_type(args, kwargs)
+        return function(*args, **kwargs)
+
+    def check_type(self, args, kwargs):
+        type_list = list(self.typemap.keys())
+        input_list = [arg for arg in args]
+        for value in kwargs.values():
+            input_list.append(value)
+        for a in type_list:
+            if len(a) == len(input_list):
+                flag = 1
+                for i in range(len(a)):
+                    if isinstance(input_list[i], a[i]) is False:
+                        flag = 0
+                if flag is 1:
+                    return self.typemap.get(a)
+        raise TypeError("no match")
+
+    # Add a new function to the type diagram
     def register(self, types, function):
         if types in self.typemap:
             raise TypeError("duplicate registration")
         self.typemap[types] = function
 
 
-#Returns a multimethod object and calls the register method
+# Returns a multimethod object and calls the register method
 def multimethod(*types):
-        def register(function):
-            # add
-            function = getattr(function, "__lastreg__", function)
-            name = function.__name__
-            mm = registry.get(name)
-            if mm is None:
-                mm = registry[name] = MultiMethod(name)
-            mm.register(types, function)
-            # add
-            mm.__lastreg__ = function
-            return mm
-        return register
+    def register(function):
+        # add
+        function = getattr(function, "__lastreg__", function)
+        name = function.__name__
+        mm = registry.get(name)
+        if mm is None:
+            mm = registry[name] = MultiMethod(name)
+        mm.register(types, function)
+        # add
+        mm.__lastreg__ = function
+        return mm
+
+    return register
